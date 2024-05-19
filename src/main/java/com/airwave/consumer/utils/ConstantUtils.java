f0802733f22267039f9a4637fe575ca9034afdb8
subham <subhams56@gmail.com>
2024-05-19 13:13:12 +0530

    Sonar Vulnerability fixes

---
+++ src/main/java/com/airwave/consumer/utils/ConstantUtils.java
@@ -0,0 +1,7 @@
+package com.airwave.consumer.utils;
+
+public class ConstantUtils {
+
+    public static final String TDSTYLE = "<td style='border: 2px solid #000000; padding: 8px;'>";
+    public static final String TD = "</td>";
+}
